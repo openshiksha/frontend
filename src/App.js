12257a32e7700a8fd4fb9db73858d747b0e8550b
Guzzler <sharangpai123@gmail.com>
2020-07-02 00:08:25 +0530

    basic setup

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          onClick={async () => {
+            const response  = await fetch('/sphinx/deal/', {
+              method: 'POST', // *GET, POST, PUT, DELETE, etc.
+              mode: 'same-origin', // no-cors, *cors, same-origin
+              cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+              credentials: 'same-origin', // include, *same-origin, omit
+              headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+              },
+              redirect: 'follow', // manual, *follow, error
+              referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            });
+            console.log(response);
+          }}
+        >
+          Call API
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
