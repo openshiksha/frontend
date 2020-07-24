94b04f7abe3fcfb46084f7042484487b5010e917
Oasis Vali <oasis.vali@gmail.com>
2020-07-24 05:10:58 -0400

    Prepare structure for deployment

--- src/index.js
+++ src/index.js
@@ -18,4 +18,4 @@ ReactDOM.render(app, document.getElementById('app_root'))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register()
+serviceWorker.unregister()
