f43bc0f7bf9c7ba17c217eadc8be5c2ec8b9a9a1
Guzzler <sharangpai123@gmail.com>
2020-08-01 02:12:11 +0530

    added basic react router implementation of pose

--- src/sphinx/components/SphinxContent.js
+++ src/sphinx/components/SphinxContent.js
@@ -4,21 +4,34 @@ import {
   Route,
   Switch
 } from 'react-router-dom'
+import posed, { PoseGroup } from 'react-pose'
 
 import PageNotFound from '../../common/error-pages/PageNotFound'
 import QuestionCreator from './sphinx-dashboard/QuestionCreator'
 import AssignmentCreator from './sphinx-dashboard/AssignmentCreator'
 
+const RouteContainer = posed.div({
+  enter: { opacity: 1, delay: 300, beforeChildren: true },
+  exit: { opacity: 0 }
+})
+
 function SphinxContent () {
   return (
-    <Row className='height-100 overflow-auto'>
-      <Switch>
-        <Route exact path="/sphinx/question" component={QuestionCreator} />
-        <Route exact path="/sphinx/assignment" component={AssignmentCreator} />
-        <Route path="/sphinx" component={QuestionCreator} />
-        <Route render={() => <PageNotFound />} />
-      </Switch>
-    </Row>
+    <Route
+      render={({ location }) => (
+        <Row className='height-100 overflow-auto'>
+          <PoseGroup>
+            <RouteContainer key={999999}>
+              <Switch location={location}>
+                <Route exact path="/sphinx/question" component={QuestionCreator} key="question"/>
+                <Route exact path="/sphinx/assignment" component={AssignmentCreator} key="assignment" />
+                <Route path="/sphinx" component={QuestionCreator} key="base" />
+                <Route render={() => <PageNotFound />} key="notFound" />
+              </Switch>
+            </RouteContainer>
+          </PoseGroup>
+        </Row>
+      )} />
   )
 }
 
